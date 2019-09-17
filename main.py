490c360068520bb9115b78181d0fbac67d28dd62
sziantoni <stefano.ziantoni20@gmail.com>
2019-09-17 19:41:49 +0200

    delete read line

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-
 from tkinter import *
 from tkinter import filedialog
 
-
 root = Tk()
-root.filename =  filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("txt","*.txt"),("all files","*.*")))
-f=open(root.filename)
-f.readline()
+root.filename = filedialog.askopenfilename(initialdir="/", title="Select file",
+                                           filetypes=(("txt", "*.txt"), ("all files", "*.*")))
+f = open(root.filename)
+
 for x in f:
     print(x)
+
 f.close()
