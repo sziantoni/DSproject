b9b33938caf56eb77ec3a8f4c7ec72c6fa026b8a
sziantoni <stefano.ziantoni20@gmail.com>
2019-09-18 18:42:12 +0200

    Strutture dati finite

--- main.py
+++ main.py
@@ -23,6 +23,10 @@ nVeicoli = f.readline()
 clients = []
 clients_pickup = []
 clients_delivery = []
+demand_pickup = []
+demand_delivery = []
+routes_pickup = [[]]
+routes_delivery = [[]]
 
 s = f.readline()
 currentLine = s.split("   ")
@@ -47,17 +51,35 @@ for i in f:
     # divisione tra delivery e pickup
     if delivery == 0:
         clients_pickup.append((float(x), float(y)))
+        demand_pickup.append(pickup)
+        if count_pickup > 0:
+            routes_pickup.append([0, count_pickup, 0])
         count_pickup += 1
     else:
         clients_delivery.append((float(x), float(y)))
+        demand_delivery.append(delivery)
+        if count_delivery > 0:
+            routes_delivery.append([0, count_pickup + count_delivery, 0])
         count_delivery += 1
 
 f.close()
+
+routes_pickup.remove([])
+routes_delivery.remove([])
+
+print(demand_pickup)
+print(demand_delivery)
+print(routes_pickup)
+print(routes_delivery)
 
 w = int(nClienti) + 1
 costs = np.array([[0 for x in range(w)] for y in range(w)])
 costs_pickup = np.array([[0 for x in range(count_pickup)] for y in range(count_pickup)])
 costs_delivery = np.array([[0 for x in range(count_delivery)] for y in range(count_delivery)])
+saving = np.array([[0 for x in range(w-1)] for y in range(w-1)])
+saving_pickup = np.array([[0 for x in range(count_pickup-1)] for y in range(count_pickup-1)])
+saving_delivery = np.array([[0 for x in range(count_delivery-1)] for y in range(count_delivery-1)])
+
 
 cont1 = 0
 cont2 = 0
@@ -100,4 +122,20 @@ for i in clients_delivery:
     cont1 += 1
     cont2 = 0
 
+count1 = 1
+count2 = 1
 
+for count1 in range(1,len(saving)+1):
+    for count2 in range(1,len(saving)+1):
+        if count1 != count2:
+            saving[count1-1, count2-1] = costs[count1][0] + costs[0][count2] - costs[count1][count2]
+    count2 = 1
+
+print("COST")
+print(costs)
+print("SAVING")
+print(saving)
+print("DEMAND DELIVERY")
+print(demand_delivery)
+print("DEMAND PICKUP")
+print(demand_pickup)
