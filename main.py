0769368175b6d62a12e0cb00d7635f8c9a55555e
sziantoni <stefano.ziantoni20@gmail.com>
2019-09-18 21:27:31 +0200

    Strutture dati finite

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from tkinter import filedialog
+
+import numpy
 import numpy as np
 
 
@@ -27,6 +29,8 @@ demand_pickup = []
 demand_delivery = []
 routes_pickup = [[]]
 routes_delivery = [[]]
+
+
 
 s = f.readline()
 currentLine = s.split("   ")
@@ -79,7 +83,9 @@ costs_delivery = np.array([[0 for x in range(count_delivery)] for y in range(cou
 saving = np.array([[0 for x in range(w-1)] for y in range(w-1)])
 saving_pickup = np.array([[0 for x in range(count_pickup-1)] for y in range(count_pickup-1)])
 saving_delivery = np.array([[0 for x in range(count_delivery-1)] for y in range(count_delivery-1)])
-
+saving_list = [()()()]
+saving_list_delivery = [()()()]
+saving_list_pickup = [()()()]
 
 cont1 = 0
 cont2 = 0
@@ -122,20 +128,69 @@ for i in clients_delivery:
     cont1 += 1
     cont2 = 0
 
-count1 = 1
-count2 = 1
-
 for count1 in range(1,len(saving)+1):
     for count2 in range(1,len(saving)+1):
         if count1 != count2:
             saving[count1-1, count2-1] = costs[count1][0] + costs[0][count2] - costs[count1][count2]
+        if saving[count1-1, count2-1] not in saving_list and len(saving_list) > 0:
+            saving_list.append(count1, count2, saving[count1-1, count2-1])
+        else:
+            if len(saving_list) == 0:
+                saving_list.append(count1, count2, saving[count1 - 1, count2 - 1])
+
     count2 = 1
+
+for count1 in range(1,len(saving_delivery)+1):
+    for count2 in range(1,len(saving_delivery)+1):
+        if count1 != count2:
+            saving_delivery[count1-1, count2-1] = costs_delivery[count1][0] + costs_delivery[0][count2] - costs_delivery[count1][count2]
+        if saving_delivery[count1 - 1, count2 - 1] not in saving_list_delivery and len(saving_list_delivery) > 0:
+            saving_list_delivery.append(count1, count2, saving_delivery[count1 - 1, count2 - 1])
+        else:
+            if len(saving_list_delivery) == 0:
+                saving_list_delivery.append(count1, count2, saving_delivery[count1 - 1, count2 - 1])
+
+    count2 = 1
+
+for count1 in range(1,len(saving_pickup)+1):
+    for count2 in range(1,len(saving_pickup)+1):
+        if count1 != count2:
+            saving_pickup[count1-1, count2-1] = costs_pickup[count1][0] + costs_pickup[0][count2] - costs_pickup[count1][count2]
+        if saving_pickup[count1 - 1, count2 - 1] not in saving_list_pickup and len(saving_list_pickup) > 0:
+            saving_list_pickup.append(count1, count2, saving_pickup[count1 - 1, count2 - 1])
+        else:
+            if len(saving_list_pickup) == 0:
+                saving_list_pickup.append(count1, count2, saving_pickup[count1 - 1, count2 - 1])
+    count2 = 1
+
+print(saving_list)
 
 print("COST")
 print(costs)
 print("SAVING")
 print(saving)
+print("SAVING D")
+print(saving_delivery)
+print("SAVING P")
+print(saving_pickup)
 print("DEMAND DELIVERY")
 print(demand_delivery)
 print("DEMAND PICKUP")
 print(demand_pickup)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
