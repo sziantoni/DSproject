279f58cae1626ba5766b7c26cc8000c617c52904
sziantoni <stefano.ziantoni20@gmail.com>
2019-09-19 18:50:28 +0200

    Commitino

--- main.py
+++ main.py
@@ -75,6 +75,7 @@ print(routes_pickup)
 print(routes_delivery)
 
 w = int(nClienti) + 1
+
 costs = np.array([[0 for x in range(w)] for y in range(w)])
 costs_pickup = np.array([[0 for x in range(count_pickup)] for y in range(count_pickup)])
 costs_delivery = np.array([[0 for x in range(count_delivery)] for y in range(count_delivery)])
@@ -146,7 +147,7 @@ for count1 in range(1, len(saving_delivery) + 1):
                                                       costs_delivery[count1][count2]
             if ((count1, count2, saving_delivery[count1 - 1, count2 - 1]) not in saving_list_delivery) and (
                     (count2, count1, saving_delivery[count1 - 1, count2 - 1]) not in saving_list_delivery) and len(
-                    saving_list_delivery) > 0:
+                saving_list_delivery) > 0:
                 saving_list_delivery.append((count1, count2, saving_delivery[count1 - 1, count2 - 1]))
             else:
                 if (len(saving_list_delivery) == 0) and (count1 != count2):
@@ -161,7 +162,7 @@ for count1 in range(1, len(saving_pickup) + 1):
                                                     costs_pickup[count1][count2]
             if ((count1, count2, saving_pickup[count1 - 1, count2 - 1]) not in saving_list_pickup) and (
                     (count2, count1, saving_pickup[count1 - 1, count2 - 1]) not in saving_list_pickup) and len(
-                    saving_list_pickup) > 0:
+                saving_list_pickup) > 0:
                 saving_list_pickup.append((count1, count2, saving_pickup[count1 - 1, count2 - 1]))
             else:
                 if (len(saving_list_pickup) == 0) and (count1 != count2):
@@ -191,3 +192,76 @@ print("DEMAND DELIVERY")
 print(demand_delivery)
 print("DEMAND PICKUP")
 print(demand_pickup)
+
+
+# LINEHAUL
+
+def CW_P(savings_list, saving_matrix, routes, capacity_limit, number_of_vehicles, demands, type):
+    start = 0
+    end = 0
+    route1 = []
+    route2 = []
+    flag1 = False
+    flag2 = False
+
+    for i in savings_list:
+        best_saving = i
+        flag1 = False
+        flag2 = False
+        counter = 0
+        for r in routes:
+            counterR = 0
+            for n in r:
+                if best_saving[0] == n and n != 0:
+                    if r[counterR + 1] == 0 and flag1 == False:
+                        start = n
+                        start_route = r[:counterR + 1]
+                        if counter != route2:
+                            route1 = counter
+                        flag1 = True
+                    else:
+                        if (r[counterR - 1] == 0) and (flag2 == False):
+                            end = n
+                            end_route = r[counterR:]
+                            if counter != route1:
+                                route2 = counter
+                            flag2 = True
+                else:
+                    if best_saving[1] == n and n != 0:
+                        if r[counterR + 1] == 0 and flag1 == False:
+                            start = n
+                            start_route = r[:counterR]
+                            if counter != route2:
+                                route1 = counter
+                            flag1 = True
+                        else:
+                            if r[counterR - 1] == 0 and flag2 == False:
+                                end = n
+                                end_route = r[counterR:]
+                                if counter != route1:
+                                    route2 = counter
+                                flag2 = True
+                counterR+=1
+        counter += 1
+        if start != 0 and end != 0:
+            if type == 0:  # presupponiamo 0 sia il pickup e 1 il delivery
+                counter_demands = 0
+                demand_total = 0
+                for d in demands:
+                    if (counter_demands + 1 in start_route) or (counter_demands in end_route):
+                        demand_total += d
+
+                if float(demand_total) <= float(capacity_limit):
+                    new_route = start_route + end_route
+                    del routes[route1]
+                    del routes[route2]
+                    routes.append(new_route)
+                    np.delete(saving, best_saving, 0)
+
+    return routes
+
+
+prova = CW_P(saving_list_pickup, saving_pickup, routes_pickup, capacity_dep, nVeicoli, demand_pickup, 0)
+
+print("PROVA")
+print(prova)
