9e5a03a7a8e45079268862aec418e24c069eadf7
sziantoni <stefano.ziantoni20@gmail.com>
2019-09-19 16:26:42 +0200

    Liste saving ordinate decrescenti senza duplicati e percorsi vuoti

--- main.py
+++ main.py
@@ -22,15 +22,13 @@ f.readline()
 
 # Lettura numero veicoli
 nVeicoli = f.readline()
-clients = [] #lista di tutti i clienti
-clients_pickup = [] #lista dei clienti con pickup
-clients_delivery = [] #lista dei clienti con delivery
-demand_pickup = [] #lista della domanda dei pickup
-demand_delivery = [] #lista della domanda dei delivery
+clients = []  # lista di tutti i clienti
+clients_pickup = []  # lista dei clienti con pickup
+clients_delivery = []  # lista dei clienti con delivery
+demand_pickup = []  # lista della domanda dei pickup
+demand_delivery = []  # lista della domanda dei delivery
 routes_pickup = [[]]
 routes_delivery = [[]]
-
-
 
 s = f.readline()
 currentLine = s.split("   ")
@@ -80,9 +78,9 @@ w = int(nClienti) + 1
 costs = np.array([[0 for x in range(w)] for y in range(w)])
 costs_pickup = np.array([[0 for x in range(count_pickup)] for y in range(count_pickup)])
 costs_delivery = np.array([[0 for x in range(count_delivery)] for y in range(count_delivery)])
-saving = np.array([[0 for x in range(w-1)] for y in range(w-1)])
-saving_pickup = np.array([[0 for x in range(count_pickup-1)] for y in range(count_pickup-1)])
-saving_delivery = np.array([[0 for x in range(count_delivery-1)] for y in range(count_delivery-1)])
+saving = np.array([[0 for x in range(w - 1)] for y in range(w - 1)])
+saving_pickup = np.array([[0 for x in range(count_pickup - 1)] for y in range(count_pickup - 1)])
+saving_delivery = np.array([[0 for x in range(count_delivery - 1)] for y in range(count_delivery - 1)])
 saving_list = []
 saving_list_delivery = []
 saving_list_pickup = []
@@ -128,42 +126,52 @@ for i in clients_delivery:
     cont1 += 1
     cont2 = 0
 
-for count1 in range(1 , len(saving)+1):
-    for count2 in range(1 ,len(saving)+1):
+for count1 in range(1, len(saving) + 1):
+    for count2 in range(1, len(saving) + 1):
         if count1 != count2:
-            saving[count1-1, count2-1] = costs[count1][0] + costs[0][count2] - costs[count1][count2]
-
-        if (np.any(saving_list[:] != saving[count1 - 1, count2 - 1])) and len(saving_list) > 0:
-            saving_list.append((count1, count2, saving[count1-1, count2-1]))
-        else:
-            if len(saving_list) == 0:
+            saving[count1 - 1, count2 - 1] = costs[count1][0] + costs[0][count2] - costs[count1][count2]
+            if ((count1, count2, saving[count1 - 1, count2 - 1]) not in saving_list) and (
+                    (count2, count1, saving[count1 - 1, count2 - 1]) not in saving_list) and len(saving_list) > 0:
                 saving_list.append((count1, count2, saving[count1 - 1, count2 - 1]))
+            else:
+                if len(saving_list) == 0:
+                    saving_list.append((count1, count2, saving[count1 - 1, count2 - 1]))
 
     count2 = 1
 
-
-for count1 in range(1,len(saving_delivery)+1):
-    for count2 in range(1,len(saving_delivery)+1):
+for count1 in range(1, len(saving_delivery) + 1):
+    for count2 in range(1, len(saving_delivery) + 1):
         if count1 != count2:
-            saving_delivery[count1-1, count2-1] = costs_delivery[count1][0] + costs_delivery[0][count2] - costs_delivery[count1][count2]
-        if (np.any(saving_list_delivery[:] != saving_delivery[count1 - 1, count2 - 1])) and len(saving_list_delivery) > 0:
-            saving_list_delivery.append((count1, count2, saving_delivery[count1 - 1, count2 - 1]))
-        else:
-            if len(saving_list_delivery) == 0:
+            saving_delivery[count1 - 1, count2 - 1] = costs_delivery[count1][0] + costs_delivery[0][count2] - \
+                                                      costs_delivery[count1][count2]
+            if ((count1, count2, saving_delivery[count1 - 1, count2 - 1]) not in saving_list_delivery) and (
+                    (count2, count1, saving_delivery[count1 - 1, count2 - 1]) not in saving_list_delivery) and len(
+                    saving_list_delivery) > 0:
                 saving_list_delivery.append((count1, count2, saving_delivery[count1 - 1, count2 - 1]))
+            else:
+                if (len(saving_list_delivery) == 0) and (count1 != count2):
+                    saving_list_delivery.append((count1, count2, saving_delivery[count1 - 1, count2 - 1]))
 
     count2 = 1
 
-for count1 in range(1,len(saving_pickup)+1):
-    for count2 in range(1,len(saving_pickup)+1):
+for count1 in range(1, len(saving_pickup) + 1):
+    for count2 in range(1, len(saving_pickup) + 1):
         if count1 != count2:
-            saving_pickup[count1-1, count2-1] = costs_pickup[count1][0] + costs_pickup[0][count2] - costs_pickup[count1][count2]
-        if (np.any(saving_list_pickup[:] != saving_pickup[count1 - 1, count2 - 1]))and len(saving_list_pickup) > 0:
-            saving_list_pickup.append((count1, count2, saving_pickup[count1 - 1, count2 - 1]))
-        else:
-            if len(saving_list_pickup) == 0:
+            saving_pickup[count1 - 1, count2 - 1] = costs_pickup[count1][0] + costs_pickup[0][count2] - \
+                                                    costs_pickup[count1][count2]
+            if ((count1, count2, saving_pickup[count1 - 1, count2 - 1]) not in saving_list_pickup) and (
+                    (count2, count1, saving_pickup[count1 - 1, count2 - 1]) not in saving_list_pickup) and len(
+                    saving_list_pickup) > 0:
                 saving_list_pickup.append((count1, count2, saving_pickup[count1 - 1, count2 - 1]))
+            else:
+                if (len(saving_list_pickup) == 0) and (count1 != count2):
+                    saving_list_pickup.append((count1, count2, saving_pickup[count1 - 1, count2 - 1]))
     count2 = 1
+
+saving_list = sorted(saving_list, key=lambda tup: tup[2], reverse=True)
+saving_list_delivery = sorted(saving_list_delivery, key=lambda tup: tup[2], reverse=True)
+saving_list_pickup = sorted(saving_list_pickup, key=lambda tup: tup[2], reverse=True)
+
 print("Saving list")
 print(saving_list)
 print("Saving list pick up")
@@ -183,20 +191,3 @@ print("DEMAND DELIVERY")
 print(demand_delivery)
 print("DEMAND PICKUP")
 print(demand_pickup)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
